b09cf45caf5a017642d63b1c274db138e7d9dbdb
musinsa-bgpark <peter.park@musinsa.com>
2023-08-02 18:51:54 +0900

    chore: root 프로젝트에서 전체 서브 모듈의 버전 관리

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,16 @@
 rootProject.name = "multiproject-demo"
 
 include("stat")
+
+pluginManagement {
+    val SPRING_BOOT_VERSION: String by settings
+    val SPRING_BOOT_DEPENDENCY_MANAGEMENT_VERSION: String by settings
+    val KOTLIN_VERSION: String by settings
+
+    plugins {
+        id("org.springframework.boot") version SPRING_BOOT_VERSION
+        id("io.spring.dependency-management") version SPRING_BOOT_DEPENDENCY_MANAGEMENT_VERSION
+        kotlin("jvm") version KOTLIN_VERSION
+        kotlin("plugin.spring") version KOTLIN_VERSION
+    }
+}
